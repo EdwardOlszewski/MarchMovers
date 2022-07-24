e4e7136eaadcb35039a32c92bd8c54f2aea3fe9e
Edward Olszewski <77998414+EdwardOlszewski@users.noreply.github.com>
2022-07-24 15:30:11 -0500

    Update index.js

--- pages/services/index.js
+++ pages/services/index.js
@@ -56,11 +56,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-export default function Services({
-  movingRates,
-  packingRates,
-  unpackingrates,
-}) {
+export default function Services() {
   const classes = useStyles()
 
   return (
@@ -268,7 +264,18 @@ export default function Services({
           </Container>
         </div>
 
-        <div style={{ marginTop: '2rem' }}>
+       
+      </>
+    </>
+  )
+}
+
+
+
+
+/*
+
+ <div style={{ marginTop: '2rem' }}>
           <Container maxWidth='lg'>
             <Typography variant='h4' className={classes.heading}>
               Our Rates
@@ -312,10 +319,7 @@ export default function Services({
             </Container>
           </Container>
         </div>
-      </>
-    </>
-  )
-}
+
 
 export const getStaticProps = async () => {
   const [movingRatesRes, packingRatesRes, unpackingRatesRes] =
@@ -333,3 +337,5 @@ export const getStaticProps = async () => {
 
   return { props: { movingRates, packingRates, unpackingrates } }
 }
+
+*/
